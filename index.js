055bdfb8a2d5c3635230ebb94c6cccefff2e3139
korot0 <108902180+korot0@users.noreply.github.com>
2022-10-17 20:30:08 -0500

    Create loop for player choice prompt until a correct choice is made

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-// ------------- TO DO -------------
-    //write a function that checks for wrong spelling/words
-
-    //Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-
 let playerPoints = 0;
 let computerPoints = 0;
+const choice = ['rock', 'paper', 'scissors'];
 
-//getComputerChoice() returns a random value from the array
+// Takes the current string(this) and capitalizes the first letter by adding '.capitalizeFirstLetter' to the string. e.g. computerSelection.capitalizeFirstLetter()
+String.prototype.capitalizeFirstLetter = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+//getComputerChoice() returns a random value from the choice array
 function getComputerChoice() {
-    let choice = ['rock', 'paper', 'scissors'];
-
-    let computer = Math.floor(Math.random() * choice.length);
+    const computer = Math.floor(Math.random() * choice.length);
     return choice[computer];
 }
 
 //playRound() prints if you win or lost depending on the choice you made at the prompt window
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return('It\'s a tie!');
+        return ('It\'s a tie!');
     } else if ( 
         (playerSelection === 'rock' && computerSelection === 'paper') 
         || 
@@ -26,28 +25,36 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === 'scissors' && computerSelection === 'rock') 
         ) {
         computerPoints++;
-        return(`You Lose! ${computerSelection} beats ${playerSelection}`);
+        return (`You Lose! ${computerSelection.capitalizeFirstLetter()} beats ${playerSelection.capitalizeFirstLetter()}`);
     } else {
         playerPoints++;
-        return(`You Win! ${playerSelection} beats ${computerSelection}`);
+        return (`You Win! ${playerSelection.capitalizeFirstLetter()} beats ${computerSelection.capitalizeFirstLetter()}`);
+    }
+}
+
+function checkPlayerChoice() {
+    let answer = false;
+    while (answer == false) {
+        const playerChoice = prompt('Rock, Paper, or Scissors?');
+        if (playerChoice == null) {
+            continue;
+        }
+        const playerChoiceInLower = playerChoice.toLowerCase();
+        if (choice.includes(playerChoiceInLower)) {
+            answer = true;
+            return playerChoiceInLower;
+        }
     }
 }
 
 //game() plays 5 rounds and announces the winner or looser at the end
 function game() {
     for (let i = 0; i < 5; i++) {
-        playerSelection = prompt('Rock, Paper, or Scissors?');
-            if (playerSelection === null) {
-            return ('Game canceled! Please refresh page to play again!');
-            } else  if (playerSelection === '') {
-                i--;
-                playerPoints--;
-                alert ('You entered an invalid selection, please try again.')
-            }
-        console.log('Player selection: ' + playerSelection);
+        playerSelection = checkPlayerChoice();
+        console.log('Player selection: ' + playerSelection.capitalizeFirstLetter());
 
         computerSelection = getComputerChoice();
-        console.log('Computer selection: ' + computerSelection);
+        console.log('Computer selection: ' + computerSelection.capitalizeFirstLetter());
         
         console.log(playRound(playerSelection, computerSelection));
 
@@ -58,11 +65,11 @@ function game() {
     }
     
     if (playerPoints > computerPoints) {
-        return('~YOU WON!~');
+        return ('~YOU WON!~');
     } else if (computerPoints > playerPoints) {
-        return('~COMPUTER WON!~');
+        return ('~COMPUTER WON!~');
     } else {
-        return('~IT\'S A TIE!~');
+        return ('~IT\'S A TIE!~');
     }
 }
 

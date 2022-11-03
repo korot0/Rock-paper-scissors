fa59ffab8ac0605293b3ebcc14ea159d84b061a6
korot0 <108902180+korot0@users.noreply.github.com>
2022-11-03 00:38:48 -0500

    Make rock, paper, scissors buttons work

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 let playerPoints = 0;
 let computerPoints = 0;
-const choice = ['rock', 'paper', 'scissors'];
 
 // Takes the current string(this) and capitalizes the first letter by adding '.capitalizeFirstLetter' to the string. e.g. computerSelection.capitalizeFirstLetter()
 String.prototype.capitalizeFirstLetter = function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-//getComputerChoice() returns a random value from the choice array
+//getComputerChoice() returns a random value from the choice array and stores the selection in computerSelection
+const choice = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice() {
     const computer = Math.floor(Math.random() * choice.length);
     return choice[computer];
 }
 
-//playRound() prints if you win or lost depending on the choice you made at the prompt window
+//player choice
+const playerChoice = document.querySelectorAll('button');
+playerChoice.forEach((selection) => {
+    selection.addEventListener('click', e => {
+        playerSelection = e.target.id;
+        console.log(playerSelection);
+        computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection))
+    });
+});
+
+//
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return ('It\'s a tie!');
@@ -32,45 +44,47 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function checkPlayerChoice() {
-    let answer = false;
-    while (answer == false) {
-        const playerChoice = prompt('Rock, Paper, or Scissors?');
-        if (playerChoice == null) {
-            continue;
-        }
-        const playerChoiceInLower = playerChoice.toLowerCase();
-        if (choice.includes(playerChoiceInLower)) {
-            answer = true;
-            return playerChoiceInLower;
-        }
-    }
-}
+
+//Gets player choice and loops until correct input is received
+// function checkPlayerChoice() {
+//     let answer = false;
+//     while (answer == false) {
+//         const playerChoice = prompt('Rock, Paper, or Scissors?');
+//         if (playerChoice == null) {
+//             continue;
+//         }
+//         const playerChoiceInLower = playerChoice.toLowerCase();
+//         if (choice.includes(playerChoiceInLower)) {
+//             answer = true;
+//             return playerChoiceInLower;
+//         }
+//     }
+// }
 
 //game() plays 5 rounds and announces the winner or looser at the end
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playerSelection = checkPlayerChoice();
-        console.log('Player selection: ' + playerSelection.capitalizeFirstLetter());
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         playerSelection = checkPlayerChoice();
+//         console.log('Player selection: ' + playerSelection.capitalizeFirstLetter());
 
-        computerSelection = getComputerChoice();
-        console.log('Computer selection: ' + computerSelection.capitalizeFirstLetter());
+//         computerSelection = getComputerChoice();
+//         console.log('Computer selection: ' + computerSelection.capitalizeFirstLetter());
         
-        console.log(playRound(playerSelection, computerSelection));
+//         console.log(playRound(playerSelection, computerSelection));
 
-        console.log('Player points: ' + playerPoints);
-        console.log('Computer points: ' + computerPoints);
+//         console.log('Player points: ' + playerPoints);
+//         console.log('Computer points: ' + computerPoints);
 
-        console.log('');
-    }
+//         console.log('');
+//     }
     
-    if (playerPoints > computerPoints) {
-        return ('~YOU WON!~');
-    } else if (computerPoints > playerPoints) {
-        return ('~COMPUTER WON!~');
-    } else {
-        return ('~IT\'S A TIE!~');
-    }
-}
+//     if (playerPoints > computerPoints) {
+//         return ('~YOU WON!~');
+//     } else if (computerPoints > playerPoints) {
+//         return ('~COMPUTER WON!~');
+//     } else {
+//         return ('~IT\'S A TIE!~');
+//     }
+// }
 
-console.log(game());
+// console.log(game());
